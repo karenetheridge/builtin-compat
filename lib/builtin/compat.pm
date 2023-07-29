c588c009264dc83cec75340aa221a2a8dfc3a6c0
Graham Knop <haarg@haarg.org>
2023-07-29 07:21:59 +0200

    change prototype of trim to ($)
    In perl 5.38, builtin::trim has been changed to have a ($) prototype.
    Change the prototype of our replacement sub to match.

--- lib/builtin/compat.pm
+++ lib/builtin/compat.pm
@@ -20,7 +20,7 @@ sub created_as_string ($);
 sub created_as_number ($);
 sub ceil ($);
 sub floor ($);
-sub trim;
+sub trim ($);
 sub indexed;
 
 BEGIN { eval { require builtin } }
@@ -100,7 +100,7 @@ sub floor ($) {
 }
 END_CODE
   trim      => sprintf(qq{#line %s "%s"\n}, __LINE__+1, __FILE__).<<'END_CODE',
-sub trim {
+sub trim ($) {
   my $string = shift;
   s/\A\s+//, s/\s+\z// for $string;
   return $string;
